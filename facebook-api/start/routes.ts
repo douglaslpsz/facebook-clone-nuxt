977150cfba72da5ba43610fa88f40e87209784ad
Douglas Lopes <douglasdmlopes@gmail.com>
2021-10-12 11:53:04 -0300

    Update de usuários

--- facebook-api/start/routes.ts
+++ facebook-api/start/routes.ts
@@ -7,6 +7,5 @@ Route.post('/users/register', 'Users/Register.store')
 Route.get('/users/register/:key', 'Users/Register.show')
 Route.put('/users/register', 'Users/Register.update')
 
-/*Route.get('/user-register', async ({ view }) => {
-  return view.render('emails/register')
-})*/
+Route.get('/users', 'Users/Main.show').middleware('auth')
+Route.put('/users', 'Users/Main.update').middleware('auth')
