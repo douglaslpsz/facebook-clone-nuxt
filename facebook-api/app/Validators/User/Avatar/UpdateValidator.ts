5c9afaa069a9a0ed36dba6b27835389fa055edec
Douglas Lopes <douglasdmlopes@gmail.com>
2021-10-13 21:23:02 -0300

    Upload de avatar

---
+++ facebook-api/app/Validators/User/Avatar/UpdateValidator.ts
@@ -0,0 +1,17 @@
+import { schema, rules } from '@ioc:Adonis/Core/Validator'
+import { HttpContextContract, rules } from '@ioc:Adonis/Core/HttpContext'
+
+export default class UpdateValidator {
+  constructor (protected ctx: HttpContextContract) {}
+
+  public schema = schema.create({
+		file: schema.file({
+      size: '5mb',
+      extnames: ['jpg', 'png', 'jpeg']
+    })
+  })
+
+  public cacheKey = this.ctx.routeKey
+
+  public messages = {}
+}
--- facebook-api/start/routes.ts
+++ facebook-api/start/routes.ts
@@ -9,3 +9,6 @@ Route.put('/users/register', 'Users/Register.update')
 
 Route.get('/users', 'Users/Main.show').middleware('auth')
 Route.put('/users', 'Users/Main.update').middleware('auth')
+
+Route.put('/users/avatar', 'Users/Avatar.update').middleware('auth')
+Route.delete('/users/avatar', 'Users/Avatar.destroy').middleware('auth')
