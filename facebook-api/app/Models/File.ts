e5114d03ba6978a78ea94c047d8ac18e7d107c1e
Douglas Lopes <douglasdmlopes@gmail.com>
2021-10-13 21:38:04 -0300

    Rota para visualizar avatar

--- facebook-api/app/Models/File.ts
+++ facebook-api/app/Models/File.ts
@@ -1,16 +1,22 @@
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, column, computed } from '@ioc:Adonis/Lucid/Orm'
+import Env from '@ioc:Adonis/Core/Env'
 
 export default class File extends BaseModel {
-  @column({ isPrimary: true })
+  @column({ isPrimary: true, serializeAs: null })
   public id: number
 
-  @column()
+  @column({ serializeAs: null })
   public fileCategory: 'Avatar' | 'Post'
 
-  @column()
+  @column({ serializeAs: null })
   public fileName: string
   
-  @column()
+  @column({ serializeAs: null })
   public ownerId: number
+
+  @computed()
+  public get url(): string {
+    return `http://${Env.get('APP_URL')}/uploads/${this.fileName}`
+  }
 
 }
--- facebook-api/start/routes.ts
+++ facebook-api/start/routes.ts
@@ -12,3 +12,5 @@ Route.put('/users', 'Users/Main.update').middleware('auth')
 
 Route.put('/users/avatar', 'Users/Avatar.update').middleware('auth')
 Route.delete('/users/avatar', 'Users/Avatar.destroy').middleware('auth')
+
+Route.get('/upload/:file', 'Uploads/Main.show')
