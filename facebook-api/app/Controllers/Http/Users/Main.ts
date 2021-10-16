133ceec490a7c4258f8777cfca9b7b3bbbfb90c8
Douglas Lopes <douglasdmlopes@gmail.com>
2021-10-16 15:53:52 -0300

    Carregando url da imagem do avatar

--- facebook-api/app/Controllers/Http/Users/Main.ts
+++ facebook-api/app/Controllers/Http/Users/Main.ts
@@ -6,6 +6,8 @@ export default class UsersController {
   public async show ({ auth }: HttpContextContract) {
     
     const user = auth.user!
+
+    await user.load('avatar')
     
     return user
   }
