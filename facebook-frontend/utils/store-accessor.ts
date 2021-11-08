d0ba7818556994ec6de3636301cd19d6ae1300cc
Douglas Lopes <douglasdmlopes@gmail.com>
2021-11-07 21:03:37 -0300

    Formilario de login

--- facebook-frontend/utils/store-accessor.ts
+++ facebook-frontend/utils/store-accessor.ts
@@ -3,14 +3,17 @@ import { getModule } from 'vuex-module-decorators'
 
 import Mobile from '@/store/mobile'
 import UserRegister from '@/store/users/register'
+import Auth from '@/store/auth'
 
 // eslint-disable-next-line import/no-mutable-exports
 let mobile: Mobile
 let userRegister: UserRegister
+let auth: Auth
 
 const initializeStores = (store: Store<any>): void => {
   mobile = getModule(Mobile, store)
   userRegister = getModule(UserRegister, store)
+  auth = getModule(Auth, store)
 }
 
-export { initializeStores, mobile, userRegister }
+export { initializeStores, mobile, userRegister, auth }
